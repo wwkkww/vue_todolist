758ab32e35a6f119924992fe2f2bb703b0f4591f
kevinW <wongkokwah@gmail.com>
2019-06-26 16:43:43 +0800

    init commit vuejs todolist

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+    <!-- <form v-on:submit.prevent="addTodo"> -->
+      <input type="text" v-model="newTitle" name="title" placeholder="Add todo...">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+// import uuid from 'uuid';
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      newTitle: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      const newTodo = {
+        // id: uuid.v4(),
+        title: this.newTitle,
+        completed: false
+      }
+      //Send to parent
+      this.$emit('add-todo', newTodo);
+      this.newTitle = "";
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type="submit"]{
+    flex: 2;
+  }
+</style>
+
+
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is a TodoList app build with Vue</p>
+  </div>
+</template>
